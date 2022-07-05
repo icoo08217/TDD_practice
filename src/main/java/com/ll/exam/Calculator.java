f3c123fa855eda74c9e99dc2b51d271337820e10
icoo08217 <icoo08216@gmail.com>
2022-07-05 15:47:32 +0900

    run 메서드명 calc로 수정/ 곱하기 메서드 구현

--- src/main/java/com/ll/exam/Calculator.java
+++ src/main/java/com/ll/exam/Calculator.java
@@ -1,19 +1,22 @@
 package com.ll.exam;
 
 public class Calculator {
-    public static int run(String s) {
+    public static int calc(String s) {
 
         boolean isMinus = s.indexOf(" - ") != -1; // - 가 포함되어 있으면 해당 인덱스의 값을 반환하는 함수 string.indexOf
         // - 가 없다면 -1을 반환한다.
+        boolean isMulti = s.indexOf(" * ") != -1;
 
         if (isMinus) {
-            return runMinus(s);
+            return calcMinus(s);
+        } else if (isMulti) {
+            return calcMulti(s);
         }
 
-        return runPlus(s);
+        return calcPlus(s);
     }
 
-    public static int runMinus(String s){
+    public static int calcMinus(String s){
         String[] sSplit = s.split(" \\- ");
         int num1 = Integer.parseInt(sSplit[0]);
         int num2 = Integer.parseInt(sSplit[1]);
@@ -21,11 +24,19 @@ public class Calculator {
         return num1 - num2;
     }
 
-    public static int runPlus(String s){
+    public static int calcPlus(String s){
         String[] sSplit = s.split(" \\+ ");
         int num1 = Integer.parseInt(sSplit[0]);
         int num2 = Integer.parseInt(sSplit[1]);
 
         return num1 + num2;
+    }
+
+    public static int calcMulti(String s){
+        String[] sSplit = s.split(" \\* ");
+        int num1 = Integer.parseInt(sSplit[0]);
+        int num2 = Integer.parseInt(sSplit[1]);
+
+        return num1 * num2;
     }
 }
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -8,28 +8,35 @@ public class AppTest {
 
     @Test
     public void 더하기() {
-        int test = Calculator.run("10 + 50");
+        int test = Calculator.calc("10 + 50");
         assertEquals(60 , test);
     }
 
     @Test
     public void 더하기_2(){
-        int test = Calculator.run("20 + 30");
+        int test = Calculator.calc("20 + 30");
 
         assertEquals(50 , test);
     }
 
     @Test
     public void 더하기_3(){
-        int test = Calculator.run("10 + 10");
+        int test = Calculator.calc("10 + 10");
 
         assertEquals(20, test);
     }
 
     @Test
     public void 빼기() {
-        int test = Calculator.run("20 - 25");
+        int test = Calculator.calc("20 - 25");
 
         assertEquals(-5, test);
+    }
+
+    @Test
+    public void 곱하기() {
+        int test = Calculator.calc("10 * 10");
+
+        assertEquals(100 , test);
     }
 }
