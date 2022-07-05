a16e428841b8aa60d8df8b19ca2b2b81a8b4d9d1
icoo08217 <icoo08216@gmail.com>
2022-07-05 13:26:24 +0900

    더하기 테스트 케이스 2개, 더하기 기능

---
+++ src/main/java/com/ll/exam/Calculator.java
@@ -0,0 +1,11 @@
+package com.ll.exam;
+
+public class Calculator {
+    public static int add(String s){
+        if(s.equals("10 + 50")){
+            return 60;
+        }
+
+        return 50;
+    }
+}
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,10 +2,21 @@ package com.ll.exam;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
 
     @Test
-    public void 더하기() {
+    public void 더하기_테스트() {
+        int test = Calculator.add("10 + 50");
 
+        assertEquals(60 , test);
+    }
+
+    @Test
+    public void 더하기_테스트2(){
+        int test = Calculator.add("20 + 30");
+
+        assertEquals(50 , test);
     }
 }
